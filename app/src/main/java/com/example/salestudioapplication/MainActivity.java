6c9f7be42edc62892f98bd3e146c825921eaba5b
Lorenzo Pisanò <lorenzopisa00@gmail.com>
2020-01-23 15:03:12 +0100

    Maps Funzionante

---
+++ app/src/main/java/com/example/salestudioapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.salestudioapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NextView();
+    }
+
+    public void NextView(){
+        final Button button = (Button) findViewById(R.id.nextPage);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MapsActivity.class));
+            }
+        });
+
+    }
+}
